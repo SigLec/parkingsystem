26664361856d965868ebb8caf379951a59e1581e
sleca <sleca@LAPTOP-UB966L17.home>
2021-05-10 11:52:18 +0200

    Ajout des TU sur package model

---
+++ src/test/java/com/parkit/parkingsystem/model/ParkingSpotTest.java
@@ -0,0 +1,44 @@
+package com.parkit.parkingsystem.model;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.parkit.parkingsystem.constants.ParkingType;
+
+class ParkingSpotTest {
+
+	private static ParkingSpot parkingSpot;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+	}
+
+	@Test
+	void testGetId() {
+		assertEquals(1, parkingSpot.getId());
+	}
+
+	@Test
+	void testGetParkingType() {
+		assertEquals(ParkingType.CAR, parkingSpot.getParkingType());
+	}
+
+	@Test
+	void testIsAvailable() {
+		assertEquals(false, parkingSpot.isAvailable());
+	}
+
+	@Test
+	void testEqualsObject() {
+		assertTrue(parkingSpot.equals(parkingSpot));
+	}
+
+	@Test
+	void testHashCode() {
+		assertEquals(1, parkingSpot.hashCode());
+	}
+}
---
+++ src/test/java/com/parkit/parkingsystem/model/TicketTest.java
@@ -0,0 +1,64 @@
+package com.parkit.parkingsystem.model;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.parkit.parkingsystem.constants.ParkingType;
+
+class TicketTest {
+
+	private static Ticket ticket;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		ticket = new Ticket();
+	}
+
+	@Test
+	void testSetId() {
+		// WHEN
+		ticket.setId(6);
+		// THEN
+		assertEquals(6, ticket.getId());
+	}
+
+	@Test
+	void testSetParkingSpot() {
+		// GIVEN
+		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+		ticket.setParkingSpot(parkingSpot);
+		// THEN
+		assertEquals(parkingSpot, ticket.getParkingSpot());
+	}
+
+	@Test
+	void testGetVehicleRegNumber() {
+		// GIVEN
+		ticket.setVehicleRegNumber("AZERTY");
+		// THEN
+		assertEquals("AZERTY", ticket.getVehicleRegNumber());
+	}
+
+	@Test
+	void testSetInTime() {
+		// GIVEN
+		Date inTime = new Date(System.currentTimeMillis());
+		ticket.setInTime(inTime);
+		// THEN
+		assertEquals(inTime, ticket.getInTime());
+	}
+
+	@Test
+	void testSetOutTime() {
+		// GIVEN
+		Date outTime = new Date(System.currentTimeMillis());
+		ticket.setOutTime(outTime);
+		// THEN
+		assertEquals(outTime, ticket.getOutTime());
+	}
+
+}
